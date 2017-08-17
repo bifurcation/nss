b3926ac896ed71f7ce548d335b26bfae1e86fa26
Richard Barnes <rlb@ipv.sx>
2017-08-16 20:26:41 -0400

    Checkpoint: CreateClientHello works

---
+++ lib/ssl/tls13txn.c
@@ -0,0 +1,275 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * TLS 1.3 Transaction Framing
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "ssl.h"
+#include "sslimpl.h"
+#include "sslproto.h"
+
+#define IO_BUFFER_SIZE (1 << 16)
+
+typedef struct {
+  uint8_t r[IO_BUFFER_SIZE];
+  uint8_t w[IO_BUFFER_SIZE];
+  int32_t r_start;
+  int32_t r_end;
+  int32_t w_start;
+} buffer_t;
+
+static void buffer_clear(buffer_t *buffer) {
+  PORT_Memset(buffer->r, 0, IO_BUFFER_SIZE);
+  PORT_Memset(buffer->w, 0, IO_BUFFER_SIZE);
+  buffer->r_start = 0;
+  buffer->r_end = 0;
+  buffer->w_start = 0;
+}
+
+/*
+
+TODO: Uncomment when used by TLSTXN functions
+
+static PRStatus buffer_write_to_read(PRFileDesc *f, void *buf, int32_t length) {
+  buffer_t *buffer = (buffer_t *) f->lower->secret;
+  if (buffer->r_end + length > IO_BUFFER_SIZE) {
+    return PR_END_OF_FILE_ERROR;
+  }
+
+  PORT_Memcpy(buffer->r + buffer->r_end, buf, length);
+  buffer->r_end += length;
+  return PR_SUCCESS;
+}
+
+static PRStatus buffer_read_all_written(PRFileDesc *f, void *buf, int32_t length_in, int32_t *length_out) {
+  buffer_t *buffer = (buffer_t *) f->lower->secret;
+  if (length_in < buffer->w_start) {
+    return PR_INSUFFICIENT_RESOURCES_ERROR;
+  }
+
+  PORT_Memcpy(buf, buffer->w, buffer->w_start);
+  return PR_SUCCESS;
+}
+*/
+
+static PRStatus buffer_close(PRFileDesc *f) {
+  buffer_t *buffer = (buffer_t *) f->secret;
+
+  f->secret = NULL;
+  free(buffer);
+  return PR_SUCCESS;
+}
+
+static PRStatus buffer_read(PRFileDesc *f, void *buf, int32_t length) {
+  buffer_t *buffer = (buffer_t *) f->secret;
+
+  if (buffer->r_end - buffer->r_start < length) {
+    return PR_WOULD_BLOCK_ERROR;
+  }
+
+  PORT_Memcpy(buf, buffer->r + buffer->r_start, length);
+  buffer->r_start += length;
+  return PR_SUCCESS;
+}
+
+static PRStatus buffer_write(PRFileDesc *f, const void *buf, int32_t length) {
+  buffer_t *buffer = (buffer_t *) f->secret;
+
+  if (buffer->w_start + length > IO_BUFFER_SIZE) {
+    return PR_WOULD_BLOCK_ERROR;
+  }
+
+  PORT_Memcpy(buffer->w + buffer->w_start, buf, length);
+  buffer->w_start += length;
+  return PR_SUCCESS;
+}
+
+static int32_t buffer_recv(PRFileDesc *f, void *buf, int32_t length,
+                           PRIntn flags, PRIntervalTime timeout) {
+  buffer_t *buffer = (buffer_t *) f->secret;
+
+  if (flags != 0) {
+    return PR_INVALID_ARGUMENT_ERROR;
+  }
+
+  int32_t to_send = length;
+  if (buffer->r_end - buffer->r_start < length) {
+    to_send = buffer->r_end - buffer->r_start;
+  }
+
+  PORT_Memcpy(buf, buffer->r + buffer->r_start, to_send);
+  buffer->r_start += to_send;
+  return to_send;
+}
+
+static int32_t buffer_send(PRFileDesc *f, const void *buf, int32_t length,
+                           PRIntn flags, PRIntervalTime timeout) {
+  buffer_t *buffer = (buffer_t *) f->secret;
+
+  if (flags != 0) {
+    return PR_INVALID_ARGUMENT_ERROR;
+  }
+
+  int32_t to_send = length;
+  if (buffer->w_start + length > IO_BUFFER_SIZE) {
+    to_send = IO_BUFFER_SIZE - buffer->w_start;
+  }
+
+  PORT_Memcpy(buffer->w + buffer->w_start, buf, to_send);
+  buffer->w_start += to_send;
+  return to_send;
+}
+
+static PRStatus buffer_getpeername(PRFileDesc *fd, PRNetAddr *addr) {
+  addr->inet.family = PR_AF_INET;
+  addr->inet.port = 0;
+  addr->inet.ip = 0;
+  return PR_SUCCESS;
+}
+
+static PRStatus buffer_getsockopt(PRFileDesc *fd, PRSocketOptionData *data) {
+  if (data->option == PR_SockOpt_Nonblocking) {
+    data->value.non_blocking = PR_TRUE;
+    return PR_SUCCESS;
+  }
+
+  return PR_FAILURE;
+}
+
+const struct PRIOMethods recorder_methods = {
+  PR_DESC_LAYERED,
+  buffer_close,       // Close
+  buffer_read,        // Read
+  buffer_write,       // Write
+  NULL,               // Available         NOT USED
+  NULL,               // Available64       NOT USED
+  NULL,               // Sync              NOT USED
+  NULL,               // Seek              NOT USED
+  NULL,               // Seek64            NOT USED
+  NULL,               // FileInfo          NOT USED
+  NULL,               // FileInfo64        NOT USED
+  NULL,               // Writev            NOT USED
+  NULL,               // Connect           NOT USED
+  NULL,               // Accept            NOT USED
+  NULL,               // Bind              NOT USED
+  NULL,               // Listen            NOT USED
+  NULL,               // Shutdown          NOT USED
+  buffer_recv,        // Recv
+  buffer_send,        // Send
+  NULL,               // Recvfrom          NOT USED
+  NULL,               // Sendto            NOT USED
+  NULL,               // Poll              NOT USED
+  NULL,               // AcceptRead        NOT USED
+  NULL,               // TransmitFile      NOT USED
+  NULL,               // Getsockname       NOT USED
+  buffer_getpeername, // Getpeername       NOT USED
+  NULL,               // Reserved
+  NULL,               // Reserved
+  buffer_getsockopt,  // Getsockoption
+  NULL,               // Setsockoption     NOT USED (?)
+  NULL,               // Sendfile          NOT USED
+  NULL,               // ConnectContinue   NOT USED
+  NULL,               // Reserved
+  NULL,               // Reserved
+  NULL,               // Reserved
+  NULL,               // Reserved
+};
+
+PRFileDesc
+*TLSTXN_Create()
+{
+  /* Create the buffer for message I/O */
+  PRDescIdentity id = PR_GetUniqueIdentity("tls_txn");
+  buffer_t *buffer = (buffer_t *) malloc(sizeof(buffer_t));
+  if (!buffer) {
+    return NULL;
+  }
+
+  buffer_clear(buffer);
+
+  /* Wrap the buffer in a file descriptor */
+  PRFileDesc *base_fd = PR_CreateIOLayerStub(id, &recorder_methods);
+  if (!base_fd) {
+    return NULL;
+  }
+
+  base_fd->secret = (PRFilePrivate *) buffer;
+
+  /* Set up the file descriptor for TLS 1.3 */
+  PRFileDesc *ssl_fd = SSL_ImportFD(NULL, base_fd);
+  if (!ssl_fd) {
+    PR_Close(base_fd);
+    return NULL;
+  }
+
+  SSLVersionRange version_range = {
+    SSL_LIBRARY_VERSION_TLS_1_3,
+    SSL_LIBRARY_VERSION_TLS_1_3
+  };
+
+  if (SSL_VersionRangeSet(ssl_fd, &version_range) != SECSuccess) {
+    PR_Close(ssl_fd);
+    return NULL;
+  }
+
+  return ssl_fd;
+}
+
+SECStatus
+TLSTXN_CreateClientHello(PRFileDesc* client_socket, SECItem *client_hello)
+{
+  SECStatus rv;
+
+  sslSocket *ss = (sslSocket *) client_socket->secret;
+  buffer_t *buffer = (buffer_t *) client_socket->lower->secret;
+
+  buffer_clear(buffer);
+
+  /* Trigger the client to begin the handshake */
+  ssl_Get1stHandshakeLock(ss)
+
+  rv = ssl_BeginClientHandshake(ss);
+  if (rv != SECSuccess) {
+    return rv;
+  }
+
+  ssl_Release1stHandshakeLock(ss);
+
+  /* At this point, the buffer should contain a ClientHello message */
+  client_hello->len = buffer->w_start;
+  client_hello->data = PORT_Alloc(client_hello->len);
+  PORT_Memcpy(client_hello->data, buffer->w, client_hello->len);
+  return SECSuccess;
+}
+
+SECStatus
+TLSTXN_HandleClientHello(PRFileDesc* server_socket,
+                         SECItem *client_hello,
+                         SECItem *server_first_flight)
+{
+  // TODO Provision first flight to read buffer
+  // TODO ssl3_Something
+  // TODO Copy out of write buffer
+  return SECFailure;
+}
+
+SECStatus
+TLSTXN_HandleServerFirstFlight(PRFileDesc* client_socket,
+                               SECItem *server_first_flight,
+                               SECItem *client_second_flight)
+{
+  // TODO Provision first flight to read buffer
+  // TODO tls13_Something
+  // TODO Copy out of write buffer
+  return SECFailure;
+}
+SECStatus
+TLSTXN_HandleClientSecondFlight(PRFileDesc* server_socket,
+                                SECItem *client_second_flight)
+{
+  // TODO Provision first flight to read buffer
+  // TODO tls13_Something
+  return SECFailure;
+}
